2810fd9b3a722e2252a92248cc55630bb2675431
Clement Kerneur <clement.dev@gmail.com>
2017-05-21 00:36:08 +0400

    Loader

--- src/react-src/components/FrontPage.js
+++ src/react-src/components/FrontPage.js
@@ -1,11 +1,32 @@
 import React from 'react'
 
 import { Link } from 'react-router-dom'
+import Loader from './common/Loader'
 
 class FrontPage extends React.Component {
+  constructor(props) {
+  	super(props)
+
+  	this.state = {
+  		loader: true
+  	}
+  }
+
+  componentDidMount() {
+  	setTimeout( () => {
+  		this.setState({
+  			loader: false
+  		});
+  	}, 3000 );
+  }
+
   render() {
     return (
-      <div>Front Page <Link to="/users">Users</Link></div>
+      <div>
+      	<Loader display={this.state.loader} />
+      	<span>Front Page</span>
+      	<Link to="/users">Users</Link>
+      </div>
     )
   }
 }
